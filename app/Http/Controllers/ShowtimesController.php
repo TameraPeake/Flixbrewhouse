7426c727c5b354c5e4977884039172faed3b318e
tamerap <tamerap@webspec.com>
2023-11-02 10:00:12 -0500

    finished begin and end time functions. looking into laravel validation to check for space between showtimes

--- app/Http/Controllers/ShowtimesController.php
+++ app/Http/Controllers/ShowtimesController.php
@@ -8,6 +8,9 @@ use Illuminate\Validation\Rule;
 
 class ShowtimesController extends Controller
 {
+    public $startTime = "00:00";
+    public $movieLength = "";
+
     //Show show create form
     public function addShowtimes() {
         // dd(request());
@@ -18,6 +21,25 @@ class ShowtimesController extends Controller
             'movies' => Movies::all()
         ]);
     }
+
+    //calculates show end time
+    public function calculateEndTime($startTime, $movieLength) {
+        $this->startTime = $startTime;
+        $this->movieLength = $movieLength;
+
+        $chosenMovieNum = preg_replace("/[^0-9]/", '', $this->movieLength); //use reg ex to grab the nums from the film
+        $grabHR = substr($chosenMovieNum, 0, 1) * 60; //set hour
+        $grabMin = substr($chosenMovieNum, 1); //set min
+        $intTotalTime = ($grabHR + $grabMin) * 60; //calc total time as in
+
+        //convert the start time to int and add the total time to it
+        $startInt = strtotime($this->startTime) + $intTotalTime;
+        $endTime = date('h:i', $startInt);
+
+        return $endTime;
+
+    }
+
 
     //createShowtimes
     public function createShowtimes(Request $request) {
@@ -31,43 +53,35 @@ class ShowtimesController extends Controller
         ]);
 
 
-        if($request->has('movieTime1')) {
-            $formFields['movieTime1Begin'] = $request->input('movieTime1');
+        if($request->has('movieTime1') && $request->input('movieTime1') != NULL) {
+            $formFields['movieTime1Begin'] = $request->input('movieTime1'); //setting begin movie time
             $chosenMovieLength = Movies::find($request->input('movies'))->length; //find the length of the film
-            $chosenMovieNum = preg_replace("/[^0-9]/", '', $chosenMovieLength); //use reg ex to grab the nums from the film
-            $grabHR = substr($chosenMovieNum, 0, 1) * 60; //set hour
-            $grabMin = substr($chosenMovieNum, 1); //set min
-            $intTotalTime = ($grabHR + $grabMin) * 100; //calc time as int
-
-            //convert the start time to int
-            $startInt = strtotime($request->input('movieTime1'));
-            $total = date("H:i:s", $startInt + $intTotalTime);
-            //$calcEndTime
-            $formFields['movieTime1End'] = $total;
+            $formFields['movieTime1End'] = $this->calculateEndTime($request->input('movieTime1'), $chosenMovieLength);
         }
+
+        if($request->has('movieTime2') && $request->input('movieTime2') != NULL) {
+            $formFields['movieTime2Begin'] = $request->input('movieTime2'); //setting begin movie time
+            $chosenMovieLength = Movies::find($request->input('movies'))->length; //find the length of the film
+            $formFields['movieTime2End'] = $this->calculateEndTime($request->input('movieTime2'), $chosenMovieLength);
+        }
+
+        if($request->has('movieTime3') && $request->input('movieTime3') != NULL) {
+            $formFields['movieTime3Begin'] = $request->input('movieTime3'); //setting begin movie time
+            $chosenMovieLength = Movies::find($request->input('movies'))->length; //find the length of the film
+            $formFields['movieTime3End'] = $this->calculateEndTime($request->input('movieTime3'), $chosenMovieLength);
+        }
+
+        if($request->has('movieTime4') && $request->input('movieTime4') != NULL) {
+            $formFields['movieTime4Begin'] = $request->input('movieTime4'); //setting begin movie time
+            $chosenMovieLength = Movies::find($request->input('movies'))->length; //find the length of the film
+            $formFields['movieTime4End'] = $this->calculateEndTime($request->input('movieTime4'), $chosenMovieLength);
+        }
+
         dd($formFields);
-        //  'movieTitle',
-        // 'movieDate',
-        // 'movieTime1Begin',
-        // 'movieTime1End',
-        // 'movieTime2Begin',
-        // 'movieTime2End',
-        // 'movieTime3Begin',
-        // 'movieTime3End',
-        // 'movieTime4Begin',
-        // 'movieTime4End'
-
-        // if($request->hasFile('poster')) {
-        //     $formFields['poster'] = $request->file('poster')->store('/poster', 'public'); //this stores it in the public/storate/poster folder
-        // }
-
-        // if($request->hasFile('banner')) {
-        //     $formFields['banner'] = $request->file('banner')->store('/banner', 'public'); //this stores it in the public/storate/banner folder
-        // }
 
 
-        // Movies::create($formFields);
+        Showtimes::create($formFields);
 
-        // return redirect('/')->with('message', 'Movie created successfully!');
+        return redirect('/')->with('message', 'Showtimes added successfully!');
     }
 }
